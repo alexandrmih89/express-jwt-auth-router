2cc46aa4655e0ad870494355f7bbb32019d34b7a
Alexander Veligurov <av@solteca.cl>
2017-10-03 13:54:30 -0300

    add isAuthenticated method to ACL

--- ACL.js
+++ ACL.js
@@ -38,6 +38,8 @@ export default (db) => {
     })
   };
 
+  acl.isAuthenticated = isAuthenticated;
+
   acl.canMiddleware = (permission, when = () => true) =>
     combine([isAuthenticated, (req, res, next) => {
       const p = permission.split(":");
