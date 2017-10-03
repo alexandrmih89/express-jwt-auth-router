2e981291d3b9f761b17cacfd169984a149ffe859
Alexander Veligurov <av@solteca.cl>
2017-10-02 22:23:57 -0300

    Add dbUtil class

---
+++ dbutil.js
@@ -0,0 +1,46 @@
+export default (Model, {
+  perPage
+} = {}) => ({
+  findAll: (req, res, next) => {
+    Model.findAll()
+      .then(items => {
+        res.result = items;
+        next();
+      })
+      .catch(next);
+  },
+  findOne: (req, res, next) => {
+    Model.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(item => {
+        res.result = item;
+        next();
+      })
+      .catch(next);
+  },
+  create: (req, res, next) => {
+    Model.create(req.body)
+      .then(item => {
+        res.result = item;
+        next();
+      })
+      .catch(next);
+  },
+  update: (req, res, next) => {
+    //TODO: single update???
+    Model.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(item => item.update(req.body))
+      .then(item => {
+        res.result = item;
+        next();
+      })
+      .catch(next);
+  }
+});
