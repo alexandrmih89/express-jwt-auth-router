87a1d804c156f7fbc711c7dd1d9a9511d68d2e53
Alexander Veligurov <av@solteca.cl>
2017-10-07 14:46:50 -0300

    acl build (get roles and relations from database)

--- ACL.js
+++ ACL.js
@@ -5,13 +5,16 @@ import SequelizeBackend from 'acl-sequelize-backend';
 import combine from './combine';
 import { isAuthenticated } from './auth';
 
+let allowed = [];
+
 //TODO: use memory or redis and load roles from database
-export default (db) => {
+export default (db, { admin = 'admin' } = {}) => {
 
   const aclOptions = {};
   const tablePrefix = 'acl_';
 
-  let acl = new ACL(new SequelizeBackend(db, tablePrefix, aclOptions));
+  //let acl = new ACL(new SequelizeBackend(db, tablePrefix, aclOptions));
+  let acl = new ACL(new ACL.memoryBackend());
 
   //TODO: this does not work as a promise :(
   acl.addUserRolesPromise = (userId, roles) => {
@@ -26,18 +29,54 @@ export default (db) => {
     })
   };
 
+  acl.push = (roles, resources, permissions) => {
+    allowed.push({ roles, resources, permissions });
+  };
+
+  acl.build = () => {
+    return db.models.role_parents.findAll().then(parentRoles => {
+      parentRoles.forEach(({ roleRole, parentRoleRole }) => {
+        acl.addRoleParents(roleRole, parentRoleRole, (err) => {
+          console.log(err, roleRole, parentRoleRole);
+        });
+      });
+    })
+      .then(() => {
+        return db.models.user_roles.findAll().then(userRoles => {
+          userRoles.forEach(({ userId, roleRole }) => {
+            acl.addUserRoles(userId, roleRole, (err) => {
+              console.warn(err, userId, roleRole);
+            });
+          });
+        });
+      })
+      .then(() => {
+        allowed.forEach(({ roles, resources, permissions }) => {
+          acl.allow(roles, resources, permissions, (err) => {
+            console.log(err, roles, resources, permissions);
+          })
+        });
+      });
+  };
+
   acl.isAuthenticated = isAuthenticated;
 
   acl.canMiddleware = (permission, when = () => true) =>
     combine([isAuthenticated, (req, res, next) => {
       const p = permission.split(":");
-      acl.isAllowed(req.user.id, p[0], p[1], (err, allowed) => {
-        if (err) {
-          next(err);
-        } else if (allowed && when(req, res)) {
+      acl.hasRole(req.user.id, admin, (err, isAdmin) => {
+        if(isAdmin) {
           next();
         } else {
-          next(new HttpError.Forbidden("Permission denied"));
+          acl.isAllowed(req.user.id, p[0], p[1], (err, allowed) => {
+            if (err) {
+              next(err);
+            } else if (allowed && when(req, res)) {
+              next();
+            } else {
+              next(new HttpError.Forbidden("Permission denied"));
+            }
+          });
         }
       });
     }]);
@@ -45,11 +84,17 @@ export default (db) => {
   acl.canSkipMiddleware = (permission, when = () => true) =>
     combine([isAuthenticated, (req, res, next) => {
       const p = permission.split(":");
-      acl.isAllowed(req.user.id, p[0], p[1], (err, allowed) => {
-        if (allowed && when(req, res)) {
+      acl.hasRole(req.user.id, admin, (err, isAdmin) => {
+        if (isAdmin) {
           next();
         } else {
-          next('route');
+          acl.isAllowed(req.user.id, p[0], p[1], (err, allowed) => {
+            if (allowed && when(req, res)) {
+              next();
+            } else {
+              next('route');
+            }
+          });
         }
       });
     }]);
