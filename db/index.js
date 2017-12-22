de4ce3d7234de6d1f06b57322935db3abff563d0
Alexander Veligurov <av@solteca.cl>
2017-12-21 21:10:02 -0300

    Export necessary models

--- db/index.js
+++ db/index.js
@@ -3,10 +3,23 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.Contact = exports.Role = exports.User = undefined;
 
 var _db = require('./db');
 
 var _db2 = _interopRequireDefault(_db);
+
+var _User = require('./User');
+
+var _User2 = _interopRequireDefault(_User);
+
+var _Role = require('./Role');
+
+var _Role2 = _interopRequireDefault(_Role);
+
+var _Contact = require('./Contact');
+
+var _Contact2 = _interopRequireDefault(_Contact);
 
 require('./User/UserScopes');
 
@@ -15,3 +28,6 @@ require('./associations');
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = _db2.default;
+exports.User = _User2.default;
+exports.Role = _Role2.default;
+exports.Contact = _Contact2.default;
